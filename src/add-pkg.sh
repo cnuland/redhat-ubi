7cc0c08c2c36e3466ce41c40cab71e05df750d3f
C.J. Nuland <cjnuland@gmail.com>
2022-05-28 19:58:28 -0400

    Update add-pkg.sh

--- src/add-pkg.sh
+++ src/add-pkg.sh
@@ -2,14 +2,7 @@
 
 source /tmp/src/build-env \
 && INSTALL_PKGS=$(cat /tmp/src/pkg-list.txt) \
-&& subscription-manager register \
-&& subscription-manager attach --pool=$BUILD_ACT_KEY \
-&& subscription-manager refresh \
-&& subscription-manager status \
 && dnf -y install $INSTALL_PKGS \
 && rpm -V --nosize --nofiledigest --nomtime --nomode $INSTALL_PKGS \
 && unset $(cat /tmp/src/build-env | cut -d'=' -f1) \
-&& dnf clean all \
-&& subscription-manager remove --all \
-&& subscription-manager unregister \
-&& subscription-manager clean
+&& dnf clean all
