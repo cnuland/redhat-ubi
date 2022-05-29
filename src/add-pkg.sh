6d427880c2975f566750de51baa36e055532718f
C.J. Nuland <cjnuland@gmail.com>
2022-05-28 20:10:32 -0400

    Update add-pkg.sh

--- src/add-pkg.sh
+++ src/add-pkg.sh
@@ -2,7 +2,7 @@
 
 source /tmp/src/build-env \
 && INSTALL_PKGS=$(cat /tmp/src/pkg-list.txt) \
-&& dnf -y install $INSTALL_PKGS \
-&& rpm -V --nosize --nofiledigest --nomtime --nomode $INSTALL_PKGS \
+&& yum --disableplugin=subscription-manager -y install $INSTALL_PKGS \
+&& yum --disableplugin=subscription-manager clean all \
 && unset $(cat /tmp/src/build-env | cut -d'=' -f1) \
 && dnf clean all
